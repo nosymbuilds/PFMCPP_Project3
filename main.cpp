71708609a6ac6b333c14bac3d3d96484702730be
nosymbuilds <77359062+nosymbuilds@users.noreply.github.com>
2021-11-09 18:28:09 +0000

    added new member functions with for/while loops and called functions in main

--- main.cpp
+++ main.cpp
@@ -76,13 +76,14 @@ struct Song
         std::string name;
         int age;
         std::string nationality;
-        int yearsActive;
         std::string mostPopularSong;
+        int salesRecord;
 
         Artist();
         void performSongLive(bool tour = true);
         void recordSongInTheStudio(bool recordAlbum);
         void promoteSong(int numberOfInterviews, bool tvAppearances);
+        void makeAHit(std::string songName, int sales);
     };
 
     Artist artistA;
@@ -97,6 +98,7 @@ struct Song
     void playSongOnRadio(int songDuration, float songTempo);
     void performSongLiveAtConcert(Artist artistB, std::string concert);
     void remixSong(std::string title, Artist artistC, std::string recordLabelSongIsSignedTo);
+    void syncToAdvert(int offer);
 };
 
 Song::Song() : duration(5), tempo(125.f), key('c'), performingArtists(1), recordLabel("Virgin")
@@ -104,7 +106,7 @@ Song::Song() : duration(5), tempo(125.f), key('c'), performingArtists(1), record
     std::cout << "Song constructed" << std::endl;
 }
 
-Song::Artist::Artist() : name("Moby"), age(50), nationality("American"), yearsActive(25), mostPopularSong("Porcelain")
+Song::Artist::Artist() : name("Moby"), age(50), nationality("American"), mostPopularSong("Porcelain"), salesRecord(20)
 {
     std::cout << "Artist constructed" << std::endl;
 }
@@ -123,6 +125,30 @@ void Song::performSongLiveAtConcert(Artist artistB, std::string concert)
 void Song::remixSong(std::string title, Artist artistC, std::string recordLabelSongIsSignedTo)
 {
     std::cout << recordLabelSongIsSignedTo << " have chosen " << artistC.name << " to remix the song " << title << std::endl;
+}
+
+void Song::syncToAdvert(int offer)
+{
+    int minimumFee = 50;
+
+    if(offer > minimumFee)
+    {
+        std::cout << "The offer of £" << offer << " to licence the track for the advert has been accepted." << std::endl;
+    }
+    else
+    {
+        for(int i = 0; i + offer <= minimumFee + 1; i++)
+        {  
+            if (i + offer > minimumFee)
+            {
+                std::cout << "The offer of £" << i + offer << " to licence the track for the advert has been accepted!" << std::endl;
+            }  
+            else 
+            {
+                std::cout << "The offer of £" << i + offer << " to licence the track for the advert has been rejected." << std::endl;
+            }
+        } 
+    }
 }
 
 void Song::Artist::performSongLive(bool tour)
@@ -161,6 +187,27 @@ void Song::Artist::promoteSong(int numberOfInterviews, bool tvAppearances)
     }  
 }
 
+void Song::Artist::makeAHit(std::string songName, int sales)
+{
+    int salesCount = 0;
+
+    for(int i = 0; i <= sales ; i++)
+    {   
+        salesCount += 1;
+    }
+
+    if(salesCount > salesRecord)
+    {
+        mostPopularSong = songName;
+        salesRecord = salesCount;
+        std::cout << "A new record has been set! " << mostPopularSong << " is now " << name << "'s most popular song with a new high sales record of " << "!" << salesRecord << std::endl;
+    }
+    else
+    {
+        std::cout << "No new records were achieved with the release of this song. " << mostPopularSong << " is still " << name << "'s most popular song with a sales record of " << salesRecord << std::endl;
+    }
+}
+
 struct Sandwich
 {
     std::string bread;
@@ -173,6 +220,7 @@ struct Sandwich
     void causeFoodPoisoning(bool uncookedMeat = true);
     void provideSatisfyingMeal(double levelOfSatisfaction, int calorieCount);
     void goMoldy(int daysWithoutStorage);
+    void eat(int bitesToEat);
 };
 
 Sandwich::Sandwich() : bread("white"), numOfFillings(1), filling("egg"), condiment("mayo"), calories(500)
@@ -198,6 +246,30 @@ void Sandwich::goMoldy(int daysWithoutStorage)
     std::cout << "After " << daysWithoutStorage << " days with no storage the sandwich has gone moldy." << std::endl;   
 }
 
+void Sandwich::eat(int bitesToEat)
+{
+    int caloriesPerBite = calories / bitesToEat;
+ 
+    for(int i = 0; i + 1 <= bitesToEat; i++)
+    {
+        if(bitesToEat - (i + 1) == bitesToEat / 2)
+            std::cout << "You are halfway through eating your sandwich!" << std::endl;
+
+        std::cout << "You had a bite of the sandwich. There are "  << bitesToEat - (i + 1) << " bites left to eat." << std::endl;
+
+        if(bitesToEat - (i + 1) == 0)
+        {
+            calories = 0;
+            std::cout << "All calories consumed." << std::endl;
+        }
+        else
+        {   
+            calories -= caloriesPerBite;
+            std::cout << "The remaining calories in the sandwich are: " << calories << std::endl;
+        }
+    }
+}
+
 struct SoccerTeam
 {
     struct Player
@@ -212,6 +284,7 @@ struct SoccerTeam
         void assistGoal(bool passToStriker = true);
         void scoreGoal(bool shootAtGoal = true);
         void celebrateGoal(std::string celebrationType, bool scorerOfGoal);
+        void playGames(int numOfGames);
     };
 
     int squadPlayers = 24;
@@ -224,6 +297,7 @@ struct SoccerTeam
     void trainSkills(Player player1, std::string skill);
     void acquireInjury(Player player2, int daysToRecover);
     void receiveAward(std::string award);
+    void sellPlayers(int playersSold);
 };
 
 SoccerTeam::SoccerTeam()
@@ -249,6 +323,23 @@ void SoccerTeam::acquireInjury(Player player3, int daysToRecover)
 void SoccerTeam::receiveAward(std::string award)
 {
     std::cout << "The team has received the " << award << " award!" << std::endl;
+}
+
+void SoccerTeam::sellPlayers(int playersSold)
+{
+    for(int i = 0; i <= playersSold - 1 ; i++)
+    {   
+        squadPlayers -= 1; 
+    }
+
+    if(squadPlayers <= 0)
+    {
+        std::cout << "All players were sold! " << std::endl;
+    }
+    else 
+    {
+        std::cout << playersSold << " players were sold. The squad size is now " << squadPlayers << std::endl;
+    }     
 }
 
 void SoccerTeam::Player::assistGoal(bool passToStriker)
@@ -287,22 +378,33 @@ void SoccerTeam::Player::celebrateGoal(std::string celebrationType, bool scorerO
     }
 }
 
+void SoccerTeam::Player::playGames(int numOfGames)
+{   
+    for(int i = 0; i <= numOfGames ; i++)
+    {   
+        gamesPlayed += 1;
+    }
+
+    std::cout << "The new total number of games played by " << name << " is " << gamesPlayed << std::endl;
+}
+
 struct Synth
 {
     int oscillators;
     int presets;
     int filters;
     int outputs;
-    std::string effectType;
+    int effectsLoaded;
 
     Synth();
     void outputMidi(int midiOutput);
     void filterAllFrequenciesAbove10k(bool moveHighCutFilterTo10k = true, bool adjustQFactor = true);
     void loadPresets(std::string presetBankName = "All");
+    void loadEffects(int numOfEffects);
 
 };
 
-Synth::Synth() : oscillators(1), presets(100), filters(3), outputs(2), effectType("delay")
+Synth::Synth() : oscillators(1), presets(100), filters(3), outputs(2), effectsLoaded(0)
 {
     std::cout << "Synth constructed" << std::endl;
 }
@@ -336,6 +438,15 @@ void Synth::loadPresets(std::string presetBankName)
     std::cout << presetBankName << " presets have been loaded." << std::endl;
 }
 
+void Synth::loadEffects(int numOfEffects)
+{   
+    for(int i = 0; i <= numOfEffects - 1 ; i++)
+    {
+        std::cout << "Effect " << i + 1 << " loaded and ready to use." << std::endl;
+        effectsLoaded++;
+    }
+}
+
 struct Seat
 {
     std::string outerMaterial = "leather";
@@ -348,8 +459,7 @@ struct Seat
     void seatRiders(int riders);
     void adjustSeatPosition(float moveVertically, float moveHorizontally);
     void heatSeat(float currentSeatTemp, float targetSeatTemp);
-
-    void print();
+    void changeSeat(double rating);
 };
 
 Seat::Seat() : riders(0)
@@ -391,6 +501,19 @@ void Seat::heatSeat( float currentSeatTemp, float targetSeatTemp)
     std::cout << "The seat temperature will be increased by " <<  increaseTemp << " to reach the desired temperature set by the rider." << std::endl;    
 }
 
+void Seat::changeSeat(double rating)
+{
+    double previousRating = safetyRating;
+
+    for(double i = 0.0; i <= rating; i += 0.1)
+    {
+        std::cout << "The mechanic is fitting the safety features of the new seat 1 screw at a time..." << std::endl;
+        safetyRating = i;
+    }
+
+    std::cout << "Your new seat has been fitted! The safety rating has changed from " << previousRating << " to " << safetyRating << std::endl;
+}
+
 struct Engine
 {
     int pistons = 2;
@@ -403,6 +526,7 @@ struct Engine
     void produceEnergy(int numOfPistons, int numOfValves, std::string engineType);
     void lubricateEngine(double availableOilInLitres = 0.9);
     void burnFuel(double availableFuelInLitres, double engineRevs);
+    void firePiston();
 };
 
 Engine::Engine()
@@ -444,6 +568,18 @@ void Engine::burnFuel(double availableFuelInLitres, double engineRevs)
     }
 }
 
+void Engine::firePiston()
+{
+    int i = 0;
+    while(i < pistons)
+    {  
+    std::cout << "Fire piston " << i + 1 << std::endl;
+    i++;
+    }
+
+    std::cout << "All pistons firing!" << std::endl;
+}
+
 struct Exhaust
 {
     double lengthInInches = 15.4;
@@ -456,6 +592,7 @@ struct Exhaust
     void turboChargeEngine(double turboCharge);
     void reduceFumes(int numOfCatalyticConverters = 1);
     void controlExhaustNoise(std::string mufflerType, float engineRevs);
+    void controlTurbo(double kph, double charge);
 };
 
 Exhaust::Exhaust()
@@ -504,6 +641,28 @@ void Exhaust::controlExhaustNoise(std::string mufflerType, float engineRevs)
     } 
 }
 
+void Exhaust::controlTurbo(double kph, double charge)
+{
+    turboChargerPower = charge;
+    double maxKph = kph * turboChargerPower;
+
+    while(kph < maxKph)
+    {
+        kph += (maxKph / 10);
+
+        if(kph > maxKph)
+        {
+            kph = maxKph;
+            std::cout << "The onboard computer reduced the kph to the maximum turbo amount. The current kph is: " << kph << std::endl;
+        }
+        else 
+        {
+            std::cout << "The current kph is: " << kph << std::endl;
+            std::cout << "The turbo increased the kph output by 10% of its maximum output."<< std::endl;
+        }
+    }          
+}
+
 struct FuelTank
 {
     double capacityInLitres = 8.5;
@@ -516,6 +675,7 @@ struct FuelTank
     void notifyFuelAmountIsLow(double fuelAmountInLitres);
     void notifyIncorrectFuelTypeUsed(bool dieselUsed = true);
     double trackFuelUsage(double previousFuelReading, double currentFuelReading);
+    void activateSensor();
 };
 
 FuelTank::FuelTank()
@@ -556,6 +716,18 @@ double FuelTank::trackFuelUsage(double previousFuelReading, double currentFuelRe
     return fuelUsed;
 }
 
+void FuelTank::activateSensor()
+{
+    int i = 0;
+    while(i < sensors)
+    {  
+        std::cout << "Activated sensor " << i + 1 << std::endl;
+        i++; 
+    }
+   
+    std::cout << "All sensors activated!" << std::endl;
+}
+
 struct Tyre
 {
     std::string material = "Synthetic Rubber";
@@ -568,6 +740,7 @@ struct Tyre
     void puncture(bool holeInTyre  = true);
     void coverWheel(int tyres = 2);
     void wear(double lifespanInKm, double distanceTravelledInKm, std::string terrain);
+    void pump(double newPressure);
 };
 
 Tyre::Tyre()
@@ -606,6 +779,29 @@ void Tyre::wear(double lifespanKm, double distanceTravelledInKm, std::string ter
     }
 }
 
+void Tyre::pump(double newPressure)
+{
+    double previousPressure = pressure;
+    //double pressureChange = newPressure - pressure;
+
+    if(newPressure - pressure >= 0)
+    {
+        while(pressure < newPressure)
+        {
+            pressure += 0.1;
+        }
+    }
+    else
+    {   
+        while(pressure > newPressure + 0.1)
+        {   
+            pressure -= 0.1;
+        }
+    }
+
+    std::cout << "The pressure has been adjusted from " << previousPressure << " to " << pressure << std::endl;
+}
+
 struct Motorbike
 {
     Seat seat;
@@ -618,6 +814,7 @@ struct Motorbike
     void move(std::string direction, int speed);
     void breakdown(std::string areaOfIssue = "Engine");
     void crash(int injuriesSustained);
+    void buy(int bikesPurchased);
 };
 
 Motorbike::Motorbike()
@@ -651,6 +848,24 @@ void Motorbike::crash(int injuriesSustained)
     }
 }
 
+void Motorbike::buy(int bikesPurchased)
+{
+    std::string garage[5];
+
+    if(bikesPurchased > 5)
+    {
+        std::cout << "You dont have room to store more than 5 bikes!" << std::endl;
+    }
+    else
+    {
+        for(int i = 0; i < bikesPurchased; i++)
+        {
+            garage[i] = std::string("Motorbike");
+            std::cout << "You stored " << garage[i] << i + 1 << " in your garage." << std::endl;
+        }
+    }   
+}
+
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -669,7 +884,9 @@ void Motorbike::crash(int injuriesSustained)
 int main()
 {
     Example::main();
-    
+
+    std::cout << std::endl;
+
     Song songA;
     Song::Artist artistA;
     songA.playSongOnRadio(5, 126.2f);
@@ -679,12 +896,19 @@ int main()
     artistA.recordSongInTheStudio(false);
     artistA.promoteSong(20, true);
     std::cout << songA.recordLabel << std::endl;
+    songA.syncToAdvert(48);
+    artistA.makeAHit("Go", 10);
+    
+    std::cout << std::endl;
 
     Sandwich salamiSandwich;
     salamiSandwich.goMoldy(2);
     salamiSandwich.provideSatisfyingMeal(5.5, 642);
     salamiSandwich.causeFoodPoisoning(true);
     std::cout << salamiSandwich.filling << std::endl;
+    salamiSandwich.eat(3);
+
+    std::cout << std::endl;
 
     SoccerTeam team1;
     SoccerTeam::Player player1;
@@ -696,6 +920,10 @@ int main()
     player1.celebrateGoal("backflip", true);
     std::cout << team1.squadPlayers << std::endl;
     std::cout << player1.gamesPlayed << std::endl;
+    player1.playGames(4);
+    team1.sellPlayers(26);
+
+    std::cout << std::endl;
 
     Synth juno;
     juno.outputMidi(2);
@@ -705,42 +933,63 @@ int main()
     juno.filterAllFrequenciesAbove10k(false, false);
     juno.loadPresets("Pads");
     std::cout << juno.presets << std::endl;
+    juno.loadEffects(4);
+
+    std::cout << std::endl;
 
     Seat seat1;
     seat1.seatRiders(0);
     seat1.adjustSeatPosition(0.2f, 0.0f);
     seat1.heatSeat(20.f, 29.7f);
     std::cout << seat1.outerMaterial << std::endl;
+    seat1.changeSeat(1.2);
+
+    std::cout << std::endl;
 
     Engine engine1;
     engine1.produceEnergy(4, 8, "combustion");
     engine1.lubricateEngine(0.9);
     engine1.burnFuel(4.2, 1.4);
     std::cout << engine1.cyclinderHeadManufacturer << std::endl;
+    engine1.firePiston();
+
+    std::cout << std::endl;
 
     Exhaust exhaust1;
     exhaust1.turboChargeEngine(2.2);
     exhaust1.reduceFumes(3);
     exhaust1.controlExhaustNoise("high", 15);
     std::cout << exhaust1.turboChargerPower<< std::endl;
+    exhaust1.controlTurbo(10.7, 6.5);
+
+    std::cout << std::endl;
 
     FuelTank fuelTank1;
     fuelTank1.notifyFuelAmountIsLow(0.2);
     fuelTank1.notifyIncorrectFuelTypeUsed();
     fuelTank1.trackFuelUsage(8.1, 0.4);
     std::cout << fuelTank1.material << std::endl;
+    fuelTank1.activateSensor();
+
+    std::cout << std::endl;
 
     Tyre tyre1;
     tyre1.puncture();
     tyre1.coverWheel(2);
     tyre1.wear(95.0, 47.5, "jungle");
     std::cout << tyre1.terrain << std::endl;
+    tyre1.pump(9.0);
+
+    std::cout << std::endl;
     
     Motorbike motorbike1;
     motorbike1.move("forwards", 40);
     motorbike1.breakdown("breaks");
     motorbike1.crash(2);
+    motorbike1.buy(6);
     std::cout << motorbike1.engine.valves << std::endl;
+
+    std::cout << std::endl;
     
     std::cout << "good to go!" << std::endl;
     
